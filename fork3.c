169b2f121353cb2d07aa7bab45e533ad70da3c52
Sandeep-v-01 <sandeep.vv.23.beai@acharya.ac.in>
2024-09-25 23:01:46 +0530

    Create fork3.c

---
+++ fork3.c
@@ -0,0 +1,10 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+int main()
+{
+printf("Before fork()\n");
+pid_t p=fork();
+printf("Hi I am a process with id:%d\n",getpid());
+printf("Done\n");
+}
